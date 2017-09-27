e8483cecf433751bb3fc71036b750bcfa32c2a7f
Martinez Calvet, Pau <Pau.Martinez@gft.com>
2017-09-27 13:28:37 +0200

    dfsd

--- gft-training/src/main/main.java
+++ gft-training/src/main/main.java
@@ -2,7 +2,7 @@ package main;
 
 public class main {
 	 public static void main(String[] args) {
-		System.out.println("holita2");
+		System.out.println("holita3");
 		System.out.println("David es manco");
 	}
 }
